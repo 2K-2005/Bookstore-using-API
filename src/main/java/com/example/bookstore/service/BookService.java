2e4320c9619d16d26d24ff90b99f3ef33eb16d1e
2K-2005 <logeshwaran11367@gmail.com>
2026-03-25 19:43:17 +0530

    Spring Boot bookstore project with use of API

---
+++ src/main/java/com/example/bookstore/service/BookService.java
@@ -0,0 +1,40 @@
+package com.example.bookstore.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.bookstore.model.*;
+import com.example.bookstore.dao.*;
+import java.util.*;
+
+@Service
+public class BookService {
+
+	@Autowired
+	BookRepo br;
+
+	//insert
+	public Book get1(Book b)
+	{
+		return br.save(b);
+	}
+	
+	//selectall
+	public List<Book> get2()
+	{
+		return br.findAll();
+	}
+	
+	//particular select
+	public Optional<Book> get3(int id)
+	{
+		return br.findById(id);
+		
+	}
+	
+	//delete by ID
+	public void get4(int id)
+	{
+		br.deleteById(id);
+	}
+	
+}
